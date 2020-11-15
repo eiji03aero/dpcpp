d1845ee955227441e139ec58e628686e33d163fc
eiji03aero <eiji03aero@gmail.com>
2020-11-16 08:44:30 +0900

    added builder

--- test/dpcpp/creational/abstract_factory.cpp
+++ test/dpcpp/creational/abstract_factory.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 
 namespace dpcpp { namespace creational {
+
 /**
  * Each distinct product of a product family should have a base interface. All
  * variants of the product must implement this interface.
